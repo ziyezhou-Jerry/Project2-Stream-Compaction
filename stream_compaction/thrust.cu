b3648918774b8606a65243bc105e2d46b17be8a9
Ziye Zhou <ziyezhou@seas.upenn.edu>
2015-09-13 14:04:54 -0400

    commit (part 4)

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -3,6 +3,7 @@
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
 #include <thrust/scan.h>
+#include <iostream>
 #include "common.h"
 #include "thrust.h"
 
@@ -16,6 +17,10 @@ void scan(int n, int *odata, const int *idata) {
     // TODO use `thrust::exclusive_scan`
     // example: for device_vectors dv_in and dv_out:
     // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+
+	thrust::exclusive_scan(idata, idata + n, odata); // in-place scan
+
+	
 }
 
 }
